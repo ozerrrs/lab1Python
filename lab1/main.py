b4a949c519ae0edbc4545a4715cd33fe41e2e392
ozerrrs <ayseozer987@gmail.com>
2023-03-07 12:42:48 +0300

    lab1python
    lab1python

---
+++ lab1/main.py
@@ -0,0 +1,39 @@
+# This is a sample Python script.
+
+# Press Shift+F10 to execute it or replace it with your code.
+# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+
+# lab 1 question 1
+x = input('What is your name?')
+print('Hello '+x)
+y = input('What is your Student ID')
+print('Your ID is ' + y)
+#lab 2 question 2
+c=int(input('Please enter variable 1'))
+d = int(input('Please enter variable 2'))
+sum = c + d
+diff = c-d
+prod = c * d
+print(sum)
+print(diff)
+print(prod)
+# lab1 question 3
+studentName = input("Enter student name: ")
+labGrade = float(input("Enter lab grade: "))
+midtermGrade = float(input("Enter midterm grade: "))
+finalGrade = float(input("Enter final grade: "))
+finalScore = labGrade * 0.25 + midtermGrade * 0.35 + finalGrade * 0.4
+print('Name: '+studentName)
+print('Last score: '+str(finalScore))
+print('Lab: '+str(labGrade))
+print('Midterm: '+ str(midtermGrade))
+#lab1 question 4
+print('*\n**\n***\n**\n*')
+
+
+
+
+
+
+
+# See PyCharm help at https://www.jetbrains.com/help/pycharm/
